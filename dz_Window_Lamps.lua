b68556fc4c97032d63f6674c42112278e65f3910
Melotron <melotron07@gmail.com>
2019-08-26 20:14:36 +0200

    Update dz_Window_Lamps.lua

--- dz_Window_Lamps.lua
+++ dz_Window_Lamps.lua
@@ -1,25 +1,28 @@
 -- Fönster lampor 
 
-return { 
-    on = { devices = { 61, 2 } 
-       },
+return {
+	on = { devices = { 61, 3 } },
 
     execute = function(domoticz)
 
+
             if domoticz.devices(61).state         == "On"     and   -- Virt_Windows
-               domoticz.devices(2).state          == "Off"    then  -- Virt Sleep
-               domoticz.devices(53).setKelvin(100)
-               domoticz.devices(53).dimTo(60).checkFirst()
---               domoticz.devices(XXX).switchOn()                     -- Not in list, switch broken.
-            end
+               domoticz.devices(3).state          == "Off"    then  -- Virt Sleep
+			   -- Send url to Livingroom
+               domoticz.devices(53).setKelvin(5000)
+               domoticz.devices(53).dimTo(40)
+			   -- Turnon Kitchen lamp
+--			   domoticz.devices(XXX).switchOn()                     -- Not in list
+			end
 			   
-            if domoticz.devices(61).state          == "On"    and   -- Virt_Windows
-               domoticz.devices(2).state           == "On"    then  -- Virt Sleep
-               domoticz.devices(53).setNightMode()
-            end
+			if domoticz.devices(61).state          == "On"    and   -- Virt_Windows
+			   domoticz.devices(3).state           == "On"    then  -- Virt Sleep
+               -- Send night light Livingroom 53
+              domoticz.devices(53).setNightMode()
+		    end
 		   
-            if domoticz.devices(61).state        == "Off"    and   -- Virt_Windows
-               domoticz.devices(2).state          == "Off"    then  -- Virt Sleep
-               domoticz.devices(53).switchOff().checkFirst()                     -- Turnoff Livingroom lamp			   
---               domoticz.devices(XXX).switchOff()                    -- Not in list, switch broken.
-            end end }
+		     if domoticz.devices(61).state        == "Off"    then  -- Virt_Windows
+			   -- Send url to Livingroom 53
+			   domoticz.devices(53).switchOff().checkFirst()                     -- Turnoff Livingroom lamp			   
+--			   domoticz.devices(XXX).switchOff()                    -- Not in list
+			end end }
