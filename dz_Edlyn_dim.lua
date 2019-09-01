6ddf9d0c48e8a3fc76092c6b08fab3f9d4aaaef9
Melotron <melotron07@gmail.com>
2019-09-01 16:16:57 +0200

    Update and rename dz_dim_up.lua to dz_Edlyn_dim.lua

---
+++ dz_Edlyn_dim.lua
@@ -0,0 +1,46 @@
+-------------------------------------------------
+-- Starts when alarm goes off on Edlyns phone. --
+-------------------------------------------------
+--
+-- It will dim up the light over the course of 
+-- 3 minutes and 15 seconds with the initial start.
+-- It dims up 1 procent every 3rd sec.
+--
+-- And after that it will return to my normal
+-- enviromental settings that runs every 1 minute.
+--
+
+return {
+	active = true,
+	on = { devices = { 76 } },
+	
+	execute = function(domoticz, device)
+		local tak = domoticz.devices(26)       -- Sets the string for tak lampan
+		local golv = domoticz.devices(25)      -- Sets the string for golv lampan
+		if (device.state == 'On') then
+		    domoticz.devices(5).switchOn()     -- Turns on the manual light, so my Env scripts dont change the light.
+		    domoticz.devices(3).switchOff()    -- Turns off the Sleep switch.
+		    domoticz.devices(43).dimTo(60)     -- Dim Hall
+		    domoticz.devices(42).dimTo(100)    -- Dim Kök
+		    tak.switchOn()
+		    tak.dimTo(1)
+		    golv.switchOn()
+		    golv.dimTo(1)
+		    
+		    for i = 1,65 do
+		        t = i * 3
+				tak.dimTo(i).afterSec(t)
+				-- golv.dimTo(i).afterSec(t)
+				-- domoticz.log('WAKE UP LIGHT timer ' ..i)
+			end
+		    
+	        -- domoticz.devices('Lamp bank links').setNightMode()
+			-- domoticz.log("WAKE UP LIGHT ACTIVE")
+		end
+		if (device.state == 'Off') then
+		domoticz.devices(5).switchOff()
+        -- domoticz.log('Morning light off, returning to normal enviroment ')
+	   	end
+		
+	end
+}
