ff775d55ed6f4e3d9fc9167469b45b3b807f6a48
Melotron <melotron07@gmail.com>
2019-09-03 18:55:39 +0200

    Update dz_Edlyn_dim.lua

--- dz_Edlyn_dim.lua
+++ dz_Edlyn_dim.lua
@@ -3,11 +3,15 @@
 -------------------------------------------------
 --
 -- It will dim up the light over the course of 
--- little over 2 minutes with the initial start.
--- It dims up 1 procent every 2nd sec.
+-- 1 minute and 30 seconds, the switch 76 will
+-- reset after 120 seconds. It dims up 1 procent 
+-- every 2nd second.
 --
 -- And after that it will return to my normal
 -- enviromental settings that runs every 1 minute.
+-- Its listening on device 76 so when it turns
+-- off so will the normal enviromental script take
+-- over and all is as it should be.
 --
 
 return {
@@ -15,30 +19,75 @@ return {
 	on = { devices = { 76 } },
 	
 	execute = function(domoticz, device)
-		local tak = domoticz.devices(26)       -- Sets the string for tak lampan
-		local golv = domoticz.devices(25)      -- Sets the string for golv lampan
-		if (device.state == 'On') then
-		    domoticz.devices(5).switchOn()     -- Turns on the manual light, so my Env scripts dont change the light.
-		    domoticz.devices(3).switchOff()    -- Turns off the Sleep switch.
-		    domoticz.devices(43).dimTo(60)     -- Dim Hall
-		    domoticz.devices(42).dimTo(100)    -- Dim Kök
-		    tak.switchOn()
-		    tak.dimTo(1)
-		    golv.switchOn()
-		    golv.dimTo(1)
+	local tak = domoticz.devices(26)       -- Sets the string for tak lampan
+	local golv = domoticz.devices(25)      -- Sets the string for golv lampan
+	local hall = domoticz.devices(43)      -- Sets the string for hall
+	local kok = domoticz.devices(42)       -- Sets the string for kök
+		
+		
+		if  (domoticz.devices(21).lux >= 3701 ) then
+	        if domoticz.devices(3).state     == "On"    and	     -- Virt_SLEEP
+	           domoticz.devices(76).state    == "On"    then     -- Morgon Ljus Edlyn
+			   
+			end end 
+		
+        if (domoticz.devices(21).lux >= 601) and (domoticz.devices(21).lux <= 3700 ) then
+	        if domoticz.devices(3).state     == "On"    and	     -- Virt_SLEEP
+	           domoticz.devices(76).state    == "On"    then     -- Morgon Ljus Edlyn  
+				  
+	           domoticz.devices(5).switchOn()     -- Turns on the manual light, so my Env scripts dont change the light.
+	           domoticz.devices(3).switchOff()    -- Turns off the Sleep switch.
+	           tak.switchOn()
+	           tak.dimTo(1)
+	           golv.switchOn()
+	           golv.dimTo(1)
+	           hall.switchOn()
+	           hall.dimTo(1)
+	           kok.switchOn()
+	           kok.dimTo(50)
 		    
-		    for i = 1,65 do
-		        t = i * 2
-				tak.dimTo(i).afterSec(t)
-				--- golv.dimTo(i).afterSec(t)
-				--- domoticz.log('WAKE UP LIGHT timer ' ..i)
-			end
+		        for i = 1,45 do
+		            t = i * 2
+				        tak.dimTo(i).afterSec(t)
+					hall.dimTo(i).afterSec(t)
+					kok.dimTo(i).afterSec(t)
+				    -- golv.dimTo(i).afterSec(t)
+				    -- domoticz.log('WAKE UP LIGHT timer ' ..i)
+			end end end 
 		    
-	        -- domoticz.devices('Lamp bank links').setNightMode()
-			-- domoticz.log("WAKE UP LIGHT ACTIVE")
-		end
-		if (device.state == 'Off') then
-		domoticz.devices(5).switchOff()
+        if (domoticz.devices(21).lux >= 0) and (domoticz.devices(21).lux <= 600) then
+	        if domoticz.devices(3).state     == "On"    and	     -- Virt_SLEEP
+	           domoticz.devices(76).state    == "On"    then     -- Morgon Ljus Edlyn  
+				  
+	           domoticz.devices(5).switchOn()     -- Turns on the manual light, so my Env scripts dont change the light.
+	           domoticz.devices(3).switchOff()    -- Turns off the Sleep switch.
+		       domoticz.devices(43).dimTo(60)     -- Dim Hall
+		       domoticz.devices(42).dimTo(100)    -- Dim Kök
+		       tak.switchOn()
+		       tak.dimTo(1)
+		       golv.switchOn()
+		       golv.dimTo(1)
+		       hall.switchOn()
+		       hall.dimTo(1)
+		       kok.switchOn()
+		       kok.dimTo(100)
+		    
+		        for i = 1,65 do
+		            t = i * 2
+				        tak.dimTo(i).afterSec(t)
+					hall.dimTo(i).afterSec(t)
+					kok.dimTo(i).afterSec(t)
+				    -- golv.dimTo(i).afterSec(t)
+				    -- domoticz.log('WAKE UP LIGHT timer ' ..i)
+			end end end
+
+
+        if domoticz.devices(3).state     == "Off"    and	     -- Virt_SLEEP
+	       domoticz.devices(76).state    == "Off"    then     -- Morgon Ljus Edlyn  
+	           
+		   domoticz.devices(5).switchOff()
         -- domoticz.log('Morning light off, returning to normal enviroment ')
-	   	end	
-	end }
+	   	end
+		
+end 
+}
