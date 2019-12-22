bb3654fec50f4433e974c6e11758a73495ea079d
Melotron <melotron07@gmail.com>
2019-12-22 12:33:18 +0100

    Update dz_Env_Storarum.lua

--- dz_Env_Storarum.lua
+++ dz_Env_Storarum.lua
@@ -7,7 +7,7 @@
 
 return {
     on = { timer   = { "every 1 minutes" },
-           devices = { 3, 5, 71, 76 }
+           devices = { 3, 5, 65, 76 }
            --  3 = Virt_Sleep
            --  5 = Virt_Manuell
            -- 71 = iDetect Anyone
@@ -17,49 +17,81 @@ return {
     execute = function(domoticz)
 
         if  (domoticz.devices(21).lux >= 3701 ) then
-            if domoticz.devices(71).state   == "On"     and	-- iDetect - Anyone
+			if domoticz.devices(65).state   == "On"     and	-- iDetect - Anyone
                domoticz.devices(3).state    == "Off"    and	-- Virt_SLEEP  
                domoticz.devices(1).state    == "Off"    and	-- Virt_Morning 
                domoticz.devices(4).state    == "Off"    and	-- Virt_TV 
                domoticz.devices(5).state   == "Off"    then	-- Virt_Manuell
                    
-               domoticz.devices(29).switchOff()                -- Storarum: Sido
-               domoticz.devices(25).switchOff()                -- Storarum: Golv
-               domoticz.devices(26).switchOff()                -- Storarum: Tak
-               domoticz.devices(44).switchOff()                -- Storarum: TV MI
-               domoticz.devices(54).switchOff()                -- Storarum: Sido RGBCCT
-               -- print("Lux mode :Day") -- Left from debug
-        end end
-
-        if (domoticz.devices(21).lux >= 601) and (domoticz.devices(21).lux <= 3700 ) then
-            if domoticz.devices(71).state   == "On"     and	-- iDetect - Anyone
+			domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
+			domoticz.devices(25).switchOff().checkFirst()   -- Storarum: Golv
+			domoticz.devices(26).switchOff().checkFirst()   -- Storarum: Tak
+			domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
+			--domoticz.devices(54).switchOff().checkFirst()   -- Storarum: Sido RGBCCT
+			-- print("Lux mode :Day") -- Left from debug
+            end end
+			
+ 	    if (domoticz.devices(21).lux >= 2001) and (domoticz.devices(21).lux <= 3700 ) then
+			if domoticz.devices(65).state   == "On"     and	-- iDetect - Anyone
                domoticz.devices(3).state    == "Off"    and	-- Virt_SLEEP  
                domoticz.devices(1).state    == "Off"    and	-- Virt_Morning 
                domoticz.devices(4).state    == "Off"    and	-- Virt_TV 
                domoticz.devices(5).state   == "Off"    then	-- Virt_Manuell 
                    
-               domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
-               domoticz.devices(25).dimTo(35)                  -- Storarum: Golv
-               domoticz.devices(26).dimTo(35)                  -- Storarum: Tak
-               domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
-               domoticz.devices(54).dimTo(50)                  -- Storarum: Sido RGBCCT
-               domoticz.devices(54).setKelvin(5000)            -- Storarum: Sido RGBCCT
-               -- print("Lux mode :Dusk") -- Left from debug
-        end end
-		
-        if   (domoticz.devices(21).lux >= 0) and (domoticz.devices(21).lux <= 600) then 
-            if domoticz.devices(71).state   == "On"     and	-- iDetect - Anyone
+			domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
+			domoticz.devices(25).dimTo(35)                  -- Storarum: Golv
+			domoticz.devices(26).dimTo(35)                  -- Storarum: Tak
+			domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
+			--domoticz.devices(54).dimTo(50)                  -- Storarum: Sido RGBCCT
+			--domoticz.devices(54).setKelvin(5000)            -- Storarum: Sido RGBCCT
+			-- print("Lux mode :Dusk") -- Left from debug
+            end end
+			
+ 	    if (domoticz.devices(21).lux >= 701) and (domoticz.devices(21).lux <= 2000 ) then
+			if domoticz.devices(65).state   == "On"     and	-- iDetect - Anyone
+               domoticz.devices(3).state    == "Off"    and	-- Virt_SLEEP  
+               domoticz.devices(1).state    == "Off"    and	-- Virt_Morning 
+               domoticz.devices(4).state    == "Off"    and	-- Virt_TV 
+               domoticz.devices(5).state   == "Off"    then	-- Virt_Manuell 
+                   
+			domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
+			domoticz.devices(25).dimTo(50)                  -- Storarum: Golv
+			domoticz.devices(26).dimTo(50)                  -- Storarum: Tak
+			domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
+			--domoticz.devices(54).dimTo(50)                  -- Storarum: Sido RGBCCT
+			--domoticz.devices(54).setKelvin(5000)            -- Storarum: Sido RGBCCT
+			-- print("Lux mode :Dusk") -- Left from debug
+            end end
+			
+       if   (domoticz.devices(21).lux >= 401) and (domoticz.devices(21).lux <= 700) then 
+			if domoticz.devices(65).state   == "On"     and	-- iDetect - Anyone
                domoticz.devices(3).state    == "Off"    and	-- Virt_SLEEP  
                domoticz.devices(1).state    == "Off"    and	-- Virt_Morning 
                domoticz.devices(4).state    == "Off"    and	-- Virt_TV 
                domoticz.devices(5).state   == "Off"    then	-- Virt_Manuell
                    
-               domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
-               domoticz.devices(25).dimTo(65)                  -- Storarum: Golv
-               domoticz.devices(26).dimTo(50)                  -- Storarum: Tak
-               domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
-               domoticz.devices(54).dimTo(50)                  -- Storarum: Sido RGBCCT
-               domoticz.devices(54).setKelvin(5000)            -- Storarum: Sido RGBCCT
-               -- print("Lux mode :Dark")   -- Left from debug
-        end end 
+			domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
+			domoticz.devices(25).dimTo(66)                  -- Storarum: Golv
+			domoticz.devices(26).dimTo(66)                  -- Storarum: Tak
+			domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
+			--domoticz.devices(54).dimTo(50)                  -- Storarum: Sido RGBCCT
+			--domoticz.devices(54).setKelvin(5000)            -- Storarum: Sido RGBCCT
+			-- print("Lux mode :Dark")   -- Left from debug
+            end end 
+		
+       if   (domoticz.devices(21).lux >= 0) and (domoticz.devices(21).lux <= 400) then 
+			if domoticz.devices(65).state   == "On"     and	-- iDetect - Anyone
+               domoticz.devices(3).state    == "Off"    and	-- Virt_SLEEP  
+               domoticz.devices(1).state    == "Off"    and	-- Virt_Morning 
+               domoticz.devices(4).state    == "Off"    and	-- Virt_TV 
+               domoticz.devices(5).state   == "Off"    then	-- Virt_Manuell
+                   
+			domoticz.devices(29).switchOff().checkFirst()   -- Storarum: Sido
+			domoticz.devices(25).dimTo(80)                  -- Storarum: Golv
+			domoticz.devices(26).dimTo(80)                  -- Storarum: Tak
+			domoticz.devices(44).switchOff().checkFirst()   -- Storarum: TV MI
+			--domoticz.devices(54).dimTo(50)                  -- Storarum: Sido RGBCCT
+			--domoticz.devices(54).setKelvin(5000)            -- Storarum: Sido RGBCCT
+			-- print("Lux mode :Dark")   -- Left from debug
+            end end 
 end  }
