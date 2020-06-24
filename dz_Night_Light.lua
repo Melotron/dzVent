408d8e3cd4316424023bf94a10ac45f1dac5760d
Melotron <melotron07@gmail.com>
2020-06-24 11:34:09 +0200

    Update dz_Night_Light.lua

--- dz_Night_Light.lua
+++ dz_Night_Light.lua
@@ -1,47 +1,47 @@
 -----------------------------------
+-- Run enviromental scene LED -----
+-----------------------------------
 -- Night Light for LED stripe -----
 -----------------------------------
 --
--- Devices that activates this script
+-- Device that activates
 --
---  3 - Virt_SLEEP
--- 58 - PIR Storarum
--- 59 - PIR Hall
--- 60 - PIR Kök
+--   3 - Virt_SLEEP
+-- 118 - PIR Storarum
+--   X - PIR Hall
+-- 128 - PIR Kök
 --
+
 	
 return {
-	on = { devices = { 59, 58, 60, 3 } },
+	on = { devices = { 118, 128 } },
 
     execute = function(domoticz)
-	
-	     if domoticz.devices(71).state     == "On"     and                     -- iDetect - Anyone
-	        domoticz.devices(3).state      == "On"     and                     --  23 = Virt_SLEEP
-	        domoticz.devices(1).state      == "Off"    then                    --  78 = Virt_Morning
 
-            if domoticz.devices(59).state          == "On"     or             -- Pir: Hall
-               domoticz.devices(58).state          == "On"     or             -- Pir: Storarum
-               domoticz.devices(60).state          == "On"     then           -- Pir: Kök
-               domoticz.devices(42).dimTo(40).checkFirst()                                 -- LED in Kitchen
-               domoticz.devices(43).dimTo(20).checkFirst()                                 -- LED in Hallway
--- Temp removed			
---               domoticz.devices(54).dimTo(10)                                 -- Lamp Livingroom
---               domoticz.devices(45).setKelvin(40)                             -- Lamp Livingroom
---               print("Motion is: On Turning on Night Light")
-	     end end
-        	
-	     if domoticz.devices(71).state     == "On"     and                     -- iDetect - Anyone
-	        domoticz.devices(3).state      == "On"     and                     --  23 = Virt_SLEEP
-	        domoticz.devices(1).state      == "Off"    then                    --  78 = Virt_Morning
+    if domoticz.devices(3).state                     == "On"      then      --  23 = Virt_SLEEP
+            if  domoticz.devices(128).state          == "Off"     then      -- Pir: Kök
+                
+                domoticz.devices(42).switchOff().afterMin(1)                -- LED in Kitchen
+                domoticz.devices(43).switchOff()                            -- LED in Hallway
+            end end	
+    
+        if domoticz.devices(3).state                 == "On"     then       --  23 = Virt_SLEEP
+            if  domoticz.devices(128).state          == "On"     then       -- Pir: Kök
+                
+                domoticz.devices(42).dimTo(20)                              -- LED in Kitchen
+                domoticz.devices(43).dimTo(5)                               -- LED in Hallway
+            end end
+    
+        if domoticz.devices(3).state                 == "On"      then      --  23 = Virt_SLEEP
+            if  domoticz.devices(118).state          == "Off"     then      -- Pir: Storarum
+                domoticz.devices(45).switchOff().afterMin(5)
+            end end
+       
+        if domoticz.devices(3).state                 == "On"     then       --  23 = Virt_SLEEP
+            if  domoticz.devices(118).state          == "On"     then       -- Pir: Storarum
+                domoticz.devices(45).dimTo(15)
+            end end
 
-            if  domoticz.devices(59).state          == "On"     or             -- Pir: Hall
-               domoticz.devices(58).state          == "On"     or             -- Pir: Storarum
-               domoticz.devices(60).state          == "On"     then           -- Pir: Kök
-               domoticz.devices(42).switchOff().afterSec(35)                  -- LED in Kitchen
-               domoticz.devices(43).switchOff().afterSec(35)                  -- LED in Hallway
--- Temp removed
---				
---               domoticz.devices(54).switchOff().afterSec(35)                  -- Lamp Livingroom
---               print("Motion is: Off: Turning off Night Light")
-	     end end	
-end }
+ 
+    end
+}
