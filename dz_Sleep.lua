b3d28d71225ca97a372dc85a5e412ebbde68578e
Melotron <melotron07@gmail.com>
2020-06-24 11:52:28 +0200

    Update dz_Sleep.lua

--- dz_Sleep.lua
+++ dz_Sleep.lua
@@ -3,31 +3,26 @@
 ---------------------------------------------
 
 return { 
-	       on = { devices = { 3, 1 } 
-       },
+	       on = { devices = { 3 } },
 
     execute = function(domoticz)
-
-        if domoticz.devices(3).state    == "On"     and     --  Virt_SLEEP 
-           domoticz.devices(1).state    == "Off"    and     --  Virt_Morning
-           domoticz.devices(5).state    == "Off"    and     --  Virt_Manuell
-           domoticz.devices(71).state   == "On"     then    --  iDetect - Anyone
-           domoticz.devices(42).switchOff().checkFirst()
-           domoticz.devices(43).switchOff().checkFirst()
-           domoticz.devices(44).switchOff().checkFirst()
-           domoticz.devices(45).switchOff().checkFirst()
-           domoticz.devices(54).switchOff().checkFirst()
-           domoticz.devices(29).switchOff().checkFirst()
-           domoticz.devices(25).switchOff().checkFirst()
-           domoticz.devices(26).switchOff().checkFirst()
---           print("Good Night")
-        end
-     
-        if domoticz.devices(3).state   == "On"     and     --  Virt_SLEEP  
-           domoticz.devices(1).state   == "On"     and     --  Virt_Morning 
-           domoticz.devices(5).state   == "Off"    and     --  Virt_Manuell
-           domoticz.devices(71).state  == "On"     then    --  iDetect - Anyone
-           domoticz.devices(1).switchOff().checkFirst()
---           print("Both Sleep and Morning are on, Turning OFF Morning switch")
-        end
-end}
+        
+        if  domoticz.devices(3).state             == "On"     and     --  Virt_SLEEP 
+	    domoticz.devices(147).state           == "On"     then    --  iDetect - Anyone
+			
+		domoticz.devices(44).switchOff()
+		domoticz.devices(45).switchOff()
+		domoticz.devices(54).switchOff()
+		domoticz.devices(25).switchOff()
+		domoticz.devices(42).switchOff()
+		domoticz.devices(26).switchOff()
+		domoticz.devices(43).switchOff()
+		domoticz.devices(5).switchOff()
+		domoticz.devices(55).switchOff()
+		domoticz.devices(94).switchOff()
+		domoticz.devices(95).switchOff()
+		domoticz.devices(101).switchOff()
+		domoticz.devices(102).switchOff()
+		domoticz.devices(93).switchOff()
+        end end
+}
