168cfe40f0613348dc7eff6427b0e6425d7bd988
Melotron <melotron07@gmail.com>
2020-11-29 21:05:45 +0100

    Update DZ_Tradfri.lua

--- DZ_Tradfri.lua
+++ DZ_Tradfri.lua
@@ -5,25 +5,38 @@ return {
 
 	execute = function(domoticz, triggeredItem)
 
-
+-- Trådfrin knapp Sovrum
         if domoticz.devices(161).state   == "B2C"    then	-- TRADFRI on/off switch T-1
-
-			domoticz.devices(3).switchOff()
+		    domoticz.devices(3).switchOn()
 		end
 		
-        if domoticz.devices(162).state   == "B2C"    then	-- TRADFRI on/off switch T-2
-
-			domoticz.devices(3).switchOff()
+		 if domoticz.devices(161).state   == "B2L"    then	-- TRADFRI on/off switch T-1
+			 domoticz.devices(86).switchOn()
+		end
+		
+        if domoticz.devices(161).state   == "B1C"    then	-- TRADFRI on/off switch T-1
+			 domoticz.devices(3).switchOff()
+		end
+		
+		if domoticz.devices(161).state   == "B1L"    then	-- TRADFRI on/off switch T-1
+			domoticz.devices(86).switchOff()
 		end
 
-        if domoticz.devices(161).state   == "B1C"    then	-- TRADFRI on/off switch T-1
-
+-- Trådfri knapp Nicole
+        if domoticz.devices(162).state   == "B2C"    then	-- TRADFRI on/off switch T-2
 			domoticz.devices(3).switchOn()
 		end
 
         if domoticz.devices(162).state   == "B1C"    then	-- TRADFRI on/off switch T-2
-
-			domoticz.devices(3).switchOn()
+			domoticz.devices(3).switchOff()
+		end
+		
+		if domoticz.devices(162).state   == "B2L"    then	-- TRADFRI on/off switch T-1
+			domoticz.devices(2).switchOn()
+		end
+		
+		if domoticz.devices(162).state   == "B1L"    then	-- TRADFRI on/off switch T-1
+			domoticz.devices(2).switchOff()
 		end
 
 	end
