c206bc5a96ab9e3702ffc2571dd9cb0004951dc4
Melotron <melotron07@gmail.com>
2020-11-29 20:40:25 +0100

    Update dz_Env_Sovrum.lua

--- dz_Env_Sovrum.lua
+++ dz_Env_Sovrum.lua
@@ -8,51 +8,56 @@
 return {
     on = { timer   = { "every 1 minutes" },
            devices = { 86, 87, 147, 76 }
-           -- 86 = Virt_SLEEP_Sovrum
-           -- 87 = Virt_Manuellt_Sovrum
-           -- 65 = iDetect Anyone
+           -- 86  = Virt_SLEEP_Sovrum 147 = iDetect - Anyone
        },
 
     execute = function(domoticz)
+    local Kelvin = domoticz.time.matchesRule('at 18:01-07:00') and 75 or 40   -- 75 during nighttime and 40 during day
+    
+-- När vi går hemmifrån
+        if domoticz.devices(150).state   == "Off"     and	    -- iDetect - Edlyn
+           domoticz.devices(151).state   == "Off"     then 	    -- iDetect - Magnus
 
-        if  (domoticz.devices(145).lux >= 18001 ) then
+           domoticz.devices(166).switchOff() 
+           domoticz.devices(180).switchOff() 
+        end
+
+-- Normalt när vi är hemma.
+        if  (domoticz.devices(145).lux >= 3001 ) then
 
 			if domoticz.devices(147).state   == "On"     and	    -- iDetect - Anyone
                domoticz.devices(86).state   == "Off"    and	    -- Virt_SLEEP_Sovrum 
                domoticz.devices(87).state   == "Off"    then	-- Virt_Manuellt_Sovrum
-                   
+                
+               domoticz.devices(166).switchOff() 
+               domoticz.devices(180).switchOff() 
 
             end end
 			
- 	    if (domoticz.devices(145).lux >= 12001) and (domoticz.devices(145).lux <= 18000 ) then
+			
+ 	    if (domoticz.devices(145).lux >= 1001) and (domoticz.devices(145).lux <= 2000 ) then
 			if domoticz.devices(147).state   == "On"     and	    -- iDetect - Anyone
                domoticz.devices(86).state   == "Off"    and	    -- Virt_SLEEP_Sovrum 
                domoticz.devices(87).state   == "Off"    then	-- Virt_Manuellt_Sovrum
                    
+               domoticz.devices(166).dimTo(10)
+               domoticz.devices(166).setKelvin(Kelvin)  
+               domoticz.devices(180).dimTo(10)
+               domoticz.devices(180).setKelvin(Kelvin)
 
             end end
 			
- 	    if (domoticz.devices(145).lux >= 1401) and (domoticz.devices(145).lux <= 12000 ) then
-			if domoticz.devices(147).state   == "On"     and	    -- iDetect - Anyone
-               domoticz.devices(86).state   == "Off"    and	    -- Virt_SLEEP_Sovrum 
-               domoticz.devices(87).state   == "Off"    then	-- Virt_Manuellt_Sovrum
-                   
-
-            end end
-			
-       if   (domoticz.devices(145).lux >= 851) and (domoticz.devices(145).lux <= 1400) then 
-			if domoticz.devices(147).state   == "On"     and	    -- iDetect - Anyone
-               domoticz.devices(86).state   == "Off"    and	    -- Virt_SLEEP_Sovrum 
-               domoticz.devices(87).state   == "Off"    then	-- Virt_Manuellt_Sovrum
-                   
-
-            end end
 		
-       if   (domoticz.devices(145).lux >= 0) and (domoticz.devices(145).lux <= 850) then 
+       if   (domoticz.devices(145).lux >= 0) and (domoticz.devices(145).lux <= 1001) then 
 			if domoticz.devices(147).state   == "On"     and	    -- iDetect - Anyone
                domoticz.devices(86).state   == "Off"    and	    -- Virt_SLEEP_Sovrum 
                domoticz.devices(87).state   == "Off"    then	-- Virt_Manuellt_Sovrum
                    
+               domoticz.devices(166).dimTo(40)
+               domoticz.devices(166).setKelvin(Kelvin)  
+               domoticz.devices(180).dimTo(40)
+               domoticz.devices(180).setKelvin(Kelvin)
 
-            end end 
+        end end 
+        
 end  }
