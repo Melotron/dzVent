ea8f6ec5f0a200ac9800867de569d36bc237863f
Melotron <melotron07@gmail.com>
2020-11-29 21:06:36 +0100

    Update dz_Night_Light.lua

--- dz_Night_Light.lua
+++ dz_Night_Light.lua
@@ -14,34 +14,35 @@
 
 	
 return {
-	on = { devices = { 118, 128 } },
+	on = { devices = {  ['Motion*'] = {'between sunset and sunrise'},} },
 
     execute = function(domoticz)
 
-    if domoticz.devices(3).state                     == "On"      then      --  23 = Virt_SLEEP
-            if  domoticz.devices(128).state          == "Off"     then      -- Pir: Kök
+    if domoticz.devices(128).state             == "Off"     and      --  PIR: Kök
+       domoticz.devices(3).state               == "On"     then      -- Virt_Sleep
                 
-                domoticz.devices(42).switchOff().afterMin(1)                -- LED in Kitchen
-                domoticz.devices(43).switchOff()                            -- LED in Hallway
-            end end	
+        domoticz.devices(42).switchOff().afterSec(20)                    -- LED in Kitchen
+        --domoticz.devices(43).switchOff()                                 -- LED in Hallway
+        end
     
-        if domoticz.devices(3).state                 == "On"     then       --  23 = Virt_SLEEP
-            if  domoticz.devices(128).state          == "On"     then       -- Pir: Kök
+    if domoticz.devices(128).state              == "On"      and       --  PIR: Kök
+       domoticz.devices(3).state                == "On"     then       -- Virt_Sleep
                 
-                domoticz.devices(42).dimTo(20)                              -- LED in Kitchen
-                domoticz.devices(43).dimTo(5)                               -- LED in Hallway
-            end end
+        domoticz.devices(42).dimTo(10)                                -- LED in Kitchen
+        --domoticz.devices(43).dimTo(5)                                 -- LED in Hallway
+        end
     
-        if domoticz.devices(3).state                 == "On"      then      --  23 = Virt_SLEEP
-            if  domoticz.devices(118).state          == "Off"     then      -- Pir: Storarum
-                domoticz.devices(45).switchOff().afterMin(5)
-            end end
+    if domoticz.devices(118).state             == "Off"     and       --  PIR: Storarum
+       domoticz.devices(3).state               == "On"     then       -- Virt_Sleep
+                
+        domoticz.devices(45).switchOff().afterMin(5)
+        end
        
-        if domoticz.devices(3).state                 == "On"     then       --  23 = Virt_SLEEP
-            if  domoticz.devices(118).state          == "On"     then       -- Pir: Storarum
-                domoticz.devices(45).dimTo(15)
-            end end
+    if domoticz.devices(118).state             == "On"      and       --  PIR: Storarum
+       domoticz.devices(3).state               == "On"     then       -- Virt_Sleep
+                
+        domoticz.devices(45).dimTo(10)
+        end
 
- 
     end
 }
